55dd8b30f16a1e1661e78f8b4bd1edcd43b4fe41
K256 <95189223+K2568@users.noreply.github.com>
2026-06-11 16:05:21 +0100

    test

--- src/main/java/com/george_vi/electroenergetics/content/railway_electrification/catenary/CatenaryHandler.java
+++ src/main/java/com/george_vi/electroenergetics/content/railway_electrification/catenary/CatenaryHandler.java
@@ -183,7 +183,7 @@ public class CatenaryHandler {
             AttachedNode node2 = e.getSecond().getSecond();
 
             double voltage = Math.abs(event.results.getVoltageAt(node1, node2));
-            boolean active = voltage > CEEConfigs.server().trainMinVoltage.get();
+            boolean active = true;
 
             double trainSpeed = Math.abs(train.speed);
 
